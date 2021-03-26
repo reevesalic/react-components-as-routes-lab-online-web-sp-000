e41d61dd9574176e3fce7ddd273a4ad8d8dbbf81
reevesalic <reevesalic@gmail.com>
2021-03-26 15:46:51 -0400

    commit tree 799a27bef837e3aeb22a5d7a50d2cd97902733bd

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -13,9 +14,16 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div className="app">
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/movies" component={Movies} />
+      </div>
     </Router>
   );
 };
+
 
 export default App
